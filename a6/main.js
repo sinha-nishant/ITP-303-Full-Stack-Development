f491a53f6d6dd6a9a0a0f0ea6a09e847614407f7
Nishant Sinha <nishant813@gmail.com>
2021-02-26 01:16:20 -0800

    Added check box and colors

--- a6/main.js
+++ a6/main.js
@@ -1,15 +1,55 @@
-$.ajax({
-	method: "GET",
-	url: "http://api.weatherbit.io/v2.0/current",
-	data: {
-		key: "cbca269f070546ab85f6e95c78f1dd49",
-		city: "Los Angeles",
-		units: "I"
+// Get weather from WeatherBit given city name
+let getWeather = function(city) {
+	$.ajax({
+		method: "GET",
+		url: "http://api.weatherbit.io/v2.0/current",
+		data: {
+			key: "cbca269f070546ab85f6e95c78f1dd49",
+			city: city,
+			units: "I"
+		}
+	}).done(
+		function(results) {
+			$("#temp").text(results.data[0].temp);
+			$("#description").text(results.data[0].weather.description);
+			$("#feels-temp").text(results.data[0].app_temp);
+		}
+	);
+}
+
+// Load weather for default selection when page loads
+$(document).ready(function() {
+	getWeather($("#location").val());
+}); 
+
+// Get weather for new location when selected location is changed
+$("#location").change(function() {
+	getWeather($("#location").val());
+});
+
+// When user clicks enter, add todo to list
+$("#todo-input").keypress(function(event) {
+	// 13 is the keyCode for the 'enter' key
+    if(event.keyCode === 13){
+		let todo = document.createElement("div");
+		todo.classList.add("todo");
+		todo.innerHTML = "<i class='fa fa-square-o'></i>" + $("#todo-input").val();
+		$("#todo-input").val("");
+        $("#todo-list").append(todo);
+		$(".todo").click(function() {
+			$(this).css("background-color", "gray");
+			$(this).css("text-decoration", "line-through");
+			$(this).css("font-style", "italic");
+		});
+    }
+});
+
+$("#hide").click(function() {
+	// let inputBox = .css("display", "none")
+	if ($("#todo-input").css("display") === "none") {
+		$("#todo-input").css("display", "inline");
 	}
-}).done(
-	function(results) {
-		// when a response is recieved
-		console.log(results);
-		$("#weather").text("Today's weather in Los Angeles: " + results.data[0].temp + "° " + results.data[0].weather.description + ". " + "Feels like " + results.data[0].app_temp + "°");
+	else {
+		$("#todo-input").css("display", "none");
 	}
-);
+});
